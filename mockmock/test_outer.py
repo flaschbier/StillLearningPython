68e1651390d4f382c04f6f8eb5847c7440f1b99e
flaschbier <developer.flaschbier@gmail.com>
2016-03-20 13:03:44 +0100

    patches work on classes either :)

--- mockmock/test_outer.py
+++ mockmock/test_outer.py
@@ -5,9 +5,9 @@ import myconst
 import outer
 
 
+@patch("myconst.get_db_name", side_effect=myconst.fun)
 class Tests(unittest.TestCase):
 
-    @patch("myconst.get_db_name", side_effect=myconst.fun)
     def test_outer(self, mo):
         """mocking where it comes from"""
         self.assertEqual(outer.functio(), myconst.TEST_VALUE)
